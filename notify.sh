7399fe6e577cbdd56e2413f864a991356f7132f4
Modesta Watercress <modestaw@ga-m5.msk.21-school.ru>
2023-01-22 14:58:42 +0300

    develop/bot

---
+++ notify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+TELEGRAM_BOT_TOKEN=5599930379:AAHYGhQGsZc0Gn1LWgQbYZLvJCGwBjR2Ky4
+TIME="10"
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+#TEXT=heeeeeyyyy
+
+TELEGRAM_USER_ID=1295996461
+TELEGRAM_USER_ID2=722828510
+
+curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
+curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID2&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
